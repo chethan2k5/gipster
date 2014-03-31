14e71a5df0d19464ddc9bd412082b57ced6b66cb
chethan <chethan2k5@gmail.com>
2014-03-31 05:13:43 +0000

    added init google maps integration page

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,11 @@
+# config/initializers/geocoder.rb
+Geocoder.configure(
+
+  # geocoding service (see below for supported options):
+  :lookup => :bing,
+
+  # to use an API key:
+  :api_key => "An5PsR-9f0BsLZ-aFmoBoOeil0XH8jp-eVd_NHuLhgRbKVf-n0XTm_m2D8ULQVhD",
+
+
+)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gipster::Application.config.secret_key_base = 'f9fdf14900e479698ac5568285db1215008a4d79aa6dcf365fa8ebc8c4840b54c8867e3a4cecfa8b42db61b122cd82aa27f05b0826776acfce3be0fdcb0ec93f'
